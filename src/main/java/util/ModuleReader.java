c93e3909d1947cea286709a9f3c9abc169f498c9
pmitsou <pavlina.mitsou@asml.com>
2019-08-21 16:59:36 +0200

    fix file seperator problem

--- src/main/java/util/ModuleReader.java
+++ src/main/java/util/ModuleReader.java
@@ -8,9 +8,9 @@ import java.util.Objects;
 public class ModuleReader {
 
     public List<String> readFromPackageName(final String packageName, final String repo) {
-        final File file = new File(repo.substring(0, repo.lastIndexOf('\\') + 1) + packageName);
+        final File file = new File(repo.substring(0, repo.lastIndexOf(File.separator) + 1) + packageName);
         final List<String> packages = Arrays.asList(Objects.requireNonNull(file.list((current, name) -> new File(current, name).isDirectory())));
-        packages.replaceAll(pack -> packageName + "/" + pack);
+        packages.replaceAll(pack -> packageName + File.separator + pack);
         return packages;
     }
 
